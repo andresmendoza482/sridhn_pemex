505bc1e9d5997e0a60ef733c005a8fd5bd3c12a4
Andrés Mendoza <ramirezmendoza482@gmail.com>
2024-02-15 17:17:16 -0600

    Terminando validaciones de formularios

--- origin/js/validations.mjs
+++ origin/js/validations.mjs
@@ -5,12 +5,12 @@ const $input = document.querySelectorAll(".section__input");
 const loginValidations = () => {
     if($form) {//Si existe un formulario ejecuta la siguiente programación.
         for(let i = 0; i < $input.length; i++) {//Generación de mensajes de validación por cada input.
-            if($input[i].getAttribute("pattern")) {//Si el input tiene el atributo "pattern" entonces tendrá un mensaje de validación.
-                const $error = document.createElement("span");
-                $error.classList.add("section__error");
-                $error.setAttribute("id", $input[i].getAttribute("name"));
-        
-                $input[i].insertAdjacentElement("afterend", $error);
+            if($input[i].required || $input[i].getAttribute("pattern")) {//Si el input tiene el atributo "required" o el atributo "pattern" entonces tendrá un mensaje de validación. 
+                const $error = document.createElement("span");//Se crea dinámicamente por cada input con alguna validación un span para el mensaje de error.
+                $error.classList.add("section__error");//Se le agrega su clase correspondiente para los estilos.
+                $error.setAttribute("id", $input[i].getAttribute("name"));//Se le agrega un id con el nombre del input al cual pertenece el error para poder indentificarlo.
+                
+                $input[i].insertAdjacentElement("afterend", $error);//Se inserta el mensaje de error en el DOM.
             }
 
         }
@@ -18,24 +18,26 @@ const loginValidations = () => {
         document.addEventListener("keyup", (event) => { 
             if(event.target.matches(".section__input")) {//Si el elemento que origina el evento es igual a lo que hay en el selector CSS.
                 const $error = document.getElementById(event.target.getAttribute("name"));
-                
+
                 if(!event.target.value) {//Si el elemento que origina el evento está vacío.
-                    console.log("No puedes dejar espacios vacíos");
-                    $error.textContent = "No puedes dejar espacios vacíos";
+                    $error.textContent = "No puedes dejar este campo vacío";//Se agrega el mensaje correspondiente al error.
                     $error.classList.add("is-active");
 
                 }else if(event.target.value){//Si el elemento que origina el evento NO está vacío.
                     $error.classList.remove("is-active");
+                    
+                    //Para esta validación el input ya no está vacío. 
+                    if(event.target.getAttribute("pattern")) {//Si el elemento que origina el evento tiene el atributo "pattern".
+                        const regex = new RegExp(event.target.getAttribute("pattern"));/*Se crea una una expresión regular 
+                        con lo que haya en el atributo "pattern" del elemento que origina el evento.*/
                         
-                    if(event.target.getAttribute("pattern")) {//Si el elemento que origina el evento está vacío y tiene el atributo "pattern".
-                        const regex = new RegExp(event.target.getAttribute("pattern"));
-                        const $error = document.getElementById(event.target.getAttribute("name"));
-                        
+                        //Si lo que hay en el elemento que origina el evento NO cumple con la expresión regular.
                         if(!regex.test(event.target.value)) {
-                            $error.textContent = event.target.dataset.error;
-                            $error.classList.add("is-active");
-                        }else {
-                            $error.classList.remove("is-active");
+                            $error.textContent = event.target.dataset.error;//El mensaje de error es igual al error que hay en su data-attribute.
+                            $error.classList.add("is-active");//Se le agrega la clase "is-active para poder mostrar el error de validación".
+
+                        }else {//En caso de que sí cumpla con la expresión regular.
+                            $error.classList.remove("is-active");//Se remueve la clase "is-active" para poder mostrar el error de la validación.
                         }
                     }
 
@@ -44,7 +46,6 @@ const loginValidations = () => {
             
         });
     }
-
 
 };
 
